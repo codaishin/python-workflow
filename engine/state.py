8e656ab11d249cc3b5b37f3ddf1a07f645a29261
codaishin <codaishin@gmail.com>
2021-09-23 22:19:16 +0200

    feat(engine): State combination and serialization
    WIP

--- engine/state.py
+++ engine/state.py
@@ -1,17 +1,52 @@
 """state"""
-
-from typing import Optional
+from typing import Iterable, Iterator, Tuple
 
 
 class State:
-    """State of a workflow"""
+    """Serialized state of a workflow process"""
 
-    def __init__(self, state: Optional[str] = None) -> None:
-        self._state = state
+    @property
+    def root(self) -> str:
+        """Root lable of this state"""
+        return self._root
+
+    @property
+    def children(self) -> Iterable["State"]:
+        """Children of the state root"""
+        return self._children
+
+    def __init__(self, root: str, children: Tuple["State", ...] = ()) -> None:
+        self._root = root
+        self._children = children
+
+    def chain_root(self, elem: "State") -> "State":
+        """Return new state with elem chained to root"""
+
+        return State(self._root, self._children + (elem,))
 
     def serialize_str(self) -> str:
         """serializes to string"""
 
-        if self._state:
-            return self._state
-        return ""
+        def children(state) -> Iterator[State]:
+            for child in state.children:
+                yield child
+
+        if self.children:
+            gen = children(self)
+            count_up = 0
+            result = self.root
+            while child := next(gen, None):
+                result += (
+                    ","
+                    + ("-" * count_up)
+                    + ("," if count_up else "")
+                    + child.root
+                )
+                count_up = 1
+                child_gen = children(child)
+                while child_child := next(child_gen, None):
+                    result += "," + child_child.root
+                    count_up = 2
+            return result
+
+        return self._root
--- engine/tests/test_state.py
+++ engine/tests/test_state.py
@@ -8,20 +8,52 @@ from ..state import State
 class TestState(TestCase):
     """test State"""
 
-    @staticmethod
-    def test_init_empty() -> None:
-        """init empty State"""
-
-        _ = State()
-
-    def test_unpack_empty(self) -> None:
-        """empty State unpacks to empty Tuple"""
-
-        state = State()
-        self.assertEqual("", state.serialize_str())
-
-    def test_unpack_single(self) -> None:
-        """single elem unpack to one elem Tuple"""
+    def test_unpack_start(self) -> None:
+        """as "start" """
 
         state = State("start")
         self.assertEqual("start", state.serialize_str())
+
+    def test_unpack_start_then_end(self) -> None:
+        """as "start,end" """
+
+        state = State("start").chain_root(State("end"))
+        self.assertEqual("start,end", state.serialize_str())
+
+    def test_unpack_start_then_two_ends(self) -> None:
+        """as "start,end_a,-,end_b" """
+
+        state = (
+            State("start")
+            .chain_root(State("end_a"))
+            .chain_root(State("end_b"))
+        )
+        self.assertEqual("start,end_a,-,end_b", state.serialize_str())
+
+    def test_unpack_start_then_two_longer_chains(self) -> None:
+        """as "start,start_a,end_a,--,start_b,end_b" """
+
+        start = State("start")
+        chain_a = State("start_a").chain_root(State("end_a"))
+        chain_b = State("start_b").chain_root(State("end_b"))
+        start = start.chain_root(chain_a).chain_root(chain_b)
+        self.assertEqual(
+            "start,start_a,end_a,--,start_b,end_b",
+            start.serialize_str(),
+        )
+
+    def test_unpack_start_then_two_even_longer_chains(self) -> None:
+        """as "start,start_a,middle_a,end_a,---,start_b,middle_b,end_b" """
+
+        start = State("start")
+        chain_a = State("start_a").chain_root(
+            State("middle_a").chain_root(State("end_a"))
+        )
+        chain_b = State("start_b").chain_root(
+            State("middle_b").chain_root(State("end_b"))
+        )
+        start = start.chain_root(chain_a).chain_root(chain_b)
+        self.assertEqual(
+            "start,start_amiddle_a,end_a,---,start_b,middle_b,end_b",
+            start.serialize_str(),
+        )

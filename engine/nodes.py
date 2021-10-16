6a602ba86d458f75586460f31a812f1d2c4ab8fb
codaishin <codaishin@gmail.com>
2021-10-16 14:08:58 +0200

    feat(nodes): Node with name

--- engine/nodes.py
+++ engine/nodes.py
@@ -1,1 +1,13 @@
 """Process nodes"""
+
+
+class Node:
+    """Node, referencing child nodes"""
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        """Node name"""
+        return self._name
--- engine/tests/test_nodes.py
+++ engine/tests/test_nodes.py
@@ -1,1 +1,20 @@
 """tests for nodes.py"""
+
+from unittest import TestCase
+
+from ..nodes import Node
+
+
+class TestNode(TestCase):
+    """Test Node"""
+
+    def test_name(self) -> None:
+        """has set name"""
+
+        node = Node("my node")
+
+        self.assertEqual("my node", node.name)
+
+        node = Node("other node")
+
+        self.assertEqual("other node", node.name)
